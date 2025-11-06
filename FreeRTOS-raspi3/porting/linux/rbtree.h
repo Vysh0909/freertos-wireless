3f0896f93773d3e3ce92d1eaacf9d105fd015b3d
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 13:42:59 +0530

    Add rb_root and rb_node stubs in rbtree.h to fix incomplete type error for bss_tree

--- FreeRTOS-raspi3/porting/linux/rbtree.h
+++ FreeRTOS-raspi3/porting/linux/rbtree.h
@@ -3,4 +3,15 @@
 
 /* Auto-generated stub header: rbtree.h */
 
+/* Minimal stub for struct rb_node and rb_root */
+struct rb_node {
+    void *rb_left;
+    void *rb_right;
+    void *rb_parent;
+};
+
+struct rb_root {
+    struct rb_node *rb_node;
+};
+
 #endif /* __RBTREE_H__ */
