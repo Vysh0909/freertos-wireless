4bc42c571cf46c5dd1caf3e84d3b27c835cc666c
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 16:19:15 +0530

    Add netdevice.h to provide dev.kobj for struct net_device

--- FreeRTOS-raspi3/porting/linux/netdevice.h
+++ FreeRTOS-raspi3/porting/linux/netdevice.h
@@ -2,9 +2,19 @@
 #define __NETDEVICE_H__
 
 /* Auto-generated stub header: netdevice.h */
+struct kobject {
+    // dummy placeholder to allow compilation
+    char dummy;
+};
+
+struct device {
+    struct kobject kobj;
+};
+
 struct net_device {
-    unsigned char dev_addr[ETH_ALEN];
+      unsigned char dev_addr[ETH_ALEN];
       bool netns_local;
+      struct device dev;
 };
 
 #endif /* __NETDEVICE_H__ */
