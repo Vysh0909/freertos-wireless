6f9ada8b4b9395dd070f77f799bad9a4d6fbdc7b
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 14:49:12 +0530

    Add define MODULE_* macros for compatibility

--- FreeRTOS-raspi3/porting/linux/module.h
+++ FreeRTOS-raspi3/porting/linux/module.h
@@ -2,5 +2,20 @@
 #define __MODULE_H__
 
 /* Auto-generated stub header: module.h */
+#ifndef MODULE_AUTHOR
+#define MODULE_AUTHOR(x)
+#endif
+
+#ifndef MODULE_LICENSE
+#define MODULE_LICENSE(x)
+#endif
+
+#ifndef MODULE_DESCRIPTION
+#define MODULE_DESCRIPTION(x)
+#endif
+
+#ifndef MODULE_VERSION
+#define MODULE_VERSION(x)
+#endif
 
 #endif /* __MODULE_H__ */
