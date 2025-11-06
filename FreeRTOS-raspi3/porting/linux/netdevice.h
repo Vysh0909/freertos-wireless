697acd03c0296911703b834c4f51e8daf67a8162
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 16:38:26 +0530

    Add net_device compat struct and IFF_DONT_BRIDGE definition for core.c

--- FreeRTOS-raspi3/porting/linux/netdevice.h
+++ FreeRTOS-raspi3/porting/linux/netdevice.h
@@ -2,6 +2,11 @@
 #define __NETDEVICE_H__
 
 /* Auto-generated stub header: netdevice.h */
+
+#ifndef IFF_DONT_BRIDGE
+#define IFF_DONT_BRIDGE 0x8000
+#endif
+
 struct kobject {
     // dummy placeholder to allow compilation
     char dummy;
@@ -19,6 +24,9 @@ struct net_device {
       unsigned char dev_addr[ETH_ALEN];
       bool netns_local;
       struct device dev;
+      unsigned long priv_flags;      /* for priv_flags usage */
+    struct wireless_dev *ieee80211_ptr; /* for ieee80211_ptr usage */
 };
+
 
 #endif /* __NETDEVICE_H__ */
