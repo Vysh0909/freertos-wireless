cea1a1aa546bd2ac629012604ce93e6b2f869ae9
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 16:45:40 +0530

    Add dummy scoped_guard macro to allow core.c compilation in porting environment

--- FreeRTOS-raspi3/porting/linux/wiphy.h
+++ FreeRTOS-raspi3/porting/linux/wiphy.h
@@ -1,9 +1,15 @@
 #ifndef __WIPHY_COMPAT_H__
 #define __WIPHY_COMPAT_H__
 
+typedef struct wiphy_work wiphy_work;
+
 /* Dummy guard macro to allow compilation */
 #ifndef guard
 #define guard(x) wiphy_compat_##x
+#endif
+
+#ifndef scoped_guard
+#define scoped_guard(x, y) if (1)
 #endif
 
 /* Dummy guard function for wiphy */
