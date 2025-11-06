1a831db00fdfc3e1d57193b3f5e32a035b5c19a5
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-06 14:39:07 +0530

    Add tracepoint macro stubs (TP_PROTO, TP_ARGS, TRACE_EVENT) to fix  compile errors

--- FreeRTOS-raspi3/porting/linux/tracepoint.h
+++ FreeRTOS-raspi3/porting/linux/tracepoint.h
@@ -2,5 +2,16 @@
 #define __TRACEPOINT_H__
 
 /* Auto-generated stub header: tracepoint.h */
+#ifndef TP_PROTO
+#define TP_PROTO(...)   /* ignore parameters */
+#endif
+
+#ifndef TP_ARGS
+#define TP_ARGS(...)    /* ignore parameters */
+#endif
+
+#ifndef TRACE_EVENT
+#define TRACE_EVENT(name, proto, args, struct, assign, print) /* nothing */
+#endif
 
 #endif /* __TRACEPOINT_H__ */
