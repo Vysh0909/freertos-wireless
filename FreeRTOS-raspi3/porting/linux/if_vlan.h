bd7269e5b1bb76dc49cfb418d35ebd8c5ea7d6db
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-07 12:04:31 +0530

    fix: add minimal if_vlan.h stub for VLAN tag support
    Added VLAN priority macros (VLAN_PRIO_MASK, VLAN_PRO_SHIFT

--- FreeRTOS-raspi3/porting/linux/if_vlan.h
+++ FreeRTOS-raspi3/porting/linux/if_vlan.h
@@ -3,4 +3,11 @@
 
 /* Auto-generated stub header: if_vlan.h */
 
+#include <stdint.h>
+#include <linux/types.h>
+
+/* VLAN priority bits (3 bits) */
+#define VLAN_PRIO_MASK   0xE000
+#define VLAN_PRIO_SHIFT  13
+
 #endif /* __IF_VLAN_H__ */
