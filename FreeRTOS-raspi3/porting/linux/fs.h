7bcb6509ff077a4b10982ccaeefeca7a70c27b1e
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-10 11:19:58 +0530

    fix(fs): correct type ordering and includes in porting fs.h
    - Added sys/types.h for ssize_t definition
    - Declared loff_t before usage
    - Reordered typedefs and struct declarations to fix “unknown type name” errors
    - Verified compilation for debugfs and core modules

---
+++ FreeRTOS-raspi3/porting/linux/fs.h
@@ -0,0 +1,36 @@
+#ifndef __PORTING_FS_H
+#define __PORTING_FS_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/types.h> 
+
+typedef long long loff_t;
+
+struct file {
+    void *private_data;
+};
+
+typedef ssize_t (*read_fn_t)(struct file *file, char *userbuf, size_t count, loff_t *ppos);
+
+struct file_operations {
+    read_fn_t read;
+    int (*open)(struct file *file);
+    loff_t (*llseek)(struct file *file, loff_t offset, int whence);
+};
+
+static inline int simple_open(struct file *file)
+{
+    (void)file;
+    return 0;
+}
+
+static inline loff_t generic_file_llseek(struct file *file, loff_t offset, int whence)
+{
+    (void)file;
+    (void)whence;
+    return offset;
+}
+
+#endif /* __PORTING_FS_H */
+
--- FreeRTOS-raspi3/porting/linux/slab.h
+++ FreeRTOS-raspi3/porting/linux/slab.h
@@ -2,5 +2,6 @@
 #define __SLAB_H__
 
 /* Auto-generated stub header: slab.h */
+#include "fs.h"
 
 #endif /* __SLAB_H__ */
