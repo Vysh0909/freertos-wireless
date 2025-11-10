988200babd537469c2dc0188cee4dcc83f6861e3
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-10 11:42:09 +0530

    add struct debugfs_cancellation definition to fix incomplete type error

--- FreeRTOS-raspi3/porting/linux/debugfs.h
+++ FreeRTOS-raspi3/porting/linux/debugfs.h
@@ -1,5 +1,10 @@
 #ifndef __LINUX_DEBUGFS_H_
 #define __LINUX_DEBUGFS_H_
 
+struct debugfs_cancellation {
+    int (*cancel)(void *data);
+    void *cancel_data;
+};
+
 #endif /* __LINUX_DEBUGFS_H_ */
 
