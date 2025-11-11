e5b892df4f3f188362d3c910de7c34c59e7bd15e
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-11 11:22:28 +0530

    porting: Add FreeRTOS-compatible nla_policy definitions
    - Added U8_MAX, U16_MAX, U32_MAX, U64_MAX macros for max value constants.
    - Defined struct nla_policy with 'type' and 'len' fields for attribute policies.
    - Added enum nla_type including NLA_U8, NLA_U16, NLA_U32, NLA_U64, NLA_S8, NLA_S16, NLA_S32, NLA_FLAG, NLA_BINARY, NLA_REJECT.
    - Implemented FreeRTOS-safe policy macros (NLA_POLICY_MIN, NLA_POLICY_MAX, NLA_POLICY_RANGE, NLA_POLICY_EXACT_LEN, NLA_POLICY_EXACT_LEN_WARN, NLA_POLICY_NESTED, NLA_POLICY_NESTED_ARRAY, NLA_POLICY_ETH_ADDR).
    - These changes allow nl80211 attribute arrays to compile on FreeRTOS without kernel dependencies or initializer errors.

--- FreeRTOS-raspi3/porting/linux/genetlink.h
+++ FreeRTOS-raspi3/porting/linux/genetlink.h
@@ -4,6 +4,50 @@
 #include <stdint.h>
 #include "netlink.h"   /* Include nlattr definition */
 #include "types.h"
+#ifndef U8_MAX
+#define U8_MAX 255
+#endif
+
+#ifndef U16_MAX
+#define U16_MAX 65535
+#endif
+
+#ifndef U32_MAX
+#define U32_MAX 0xFFFFFFFF
+#endif
+
+#ifndef U64_MAX
+#define U64_MAX 0xFFFFFFFFFFFFFFFFULL
+#endif
+
+struct nla_policy {
+    u16 type;
+    u16 len;   /* optional: used for binary or max value validation */
+};
+
+enum nla_type {
+    NLA_UNSPEC = 0,
+    NLA_U8,
+    NLA_U16,
+    NLA_U32,
+    NLA_U64,
+    NLA_S8,
+    NLA_S16,
+    NLA_S32,
+    NLA_FLAG,
+    NLA_BINARY,
+    NLA_REJECT
+};
+
+#define NLA_POLICY_MIN(type_enum, minval)    ((struct nla_policy){ .type = type_enum, .len = minval })
+#define NLA_POLICY_MAX(type_enum, maxval)    ((struct nla_policy){ .type = type_enum, .len = maxval })
+#define NLA_POLICY_RANGE(type_enum, minval, maxval) ((struct nla_policy){ .type = type_enum, .len = maxval })
+#define NLA_POLICY_EXACT_LEN(lenval)            ((struct nla_policy){ .len = lenval })
+#define NLA_POLICY_EXACT_LEN_WARN(lenval)       ((struct nla_policy){ .len = lenval })
+#define NLA_POLICY_NESTED(array)                ((struct nla_policy){ .type = NLA_BINARY })
+#define NLA_POLICY_NESTED_ARRAY(array)          ((struct nla_policy){ .type = NLA_BINARY })
+#define NLA_POLICY_ETH_ADDR                      ((struct nla_policy){ .len = 6 })
+
 
 struct netlink_ext_ack {
     const char *msg;
