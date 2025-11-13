3a3791fa63a06046791130b899141a675b276133
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-13 11:29:42 +0530

    Stub Linux debugfs types and macros for freestanding/FreeRTOS build to allow compilation without kernel dependencies.

--- FreeRTOS-raspi3/porting/linux/debugfs.h
+++ FreeRTOS-raspi3/porting/linux/debugfs.h
@@ -1,6 +1,8 @@
 #ifndef __LINUX_DEBUGFS_H_
 #define __LINUX_DEBUGFS_H_
 
+#include "fs.h"
+
 struct debugfs_cancellation {
     int (*cancel)(void *data);
     void *cancel_data;
@@ -9,6 +11,24 @@ struct debugfs_cancellation {
 #ifndef debugfs_remove
 #define debugfs_remove(entry) ((void)0)
 #endif
+
+#define __user
+
+/* Dummy struct to allow compilation */
+struct debugfs_short_fops {
+    void *read;
+    void *llseek;
+};
+
+#define DEBUGFS_READONLY_FILE_FN(name, fmt, value) \
+    static ssize_t name##_read(void *file, char *userbuf, size_t count) { return 0; }
+
+#define DEBUGFS_READONLY_FILE_OPS(name) \
+    static const struct debugfs_short_fops name##_ops = { NULL, NULL }
+
+#define DEBUGFS_READONLY_FILE(name, fmt, value) \
+    DEBUGFS_READONLY_FILE_FN(name, fmt, value); \
+    DEBUGFS_READONLY_FILE_OPS(name)
 
 #endif /* __LINUX_DEBUGFS_H_ */
 
--- FreeRTOS-raspi3/porting/linux/idr.h
+++ FreeRTOS-raspi3/porting/linux/idr.h
@@ -1,4 +1,9 @@
 #ifndef __LINUX_IDR_H_
 #define __LINUX_IDR_H_
 
+#ifndef idr_for_each_entry
+#define idr_for_each_entry(idr, entry, id) \
+    for ((entry) = NULL; 0; (entry) = NULL)
+#endif
+
 #endif /* __LINUX_IDR_H_ */
