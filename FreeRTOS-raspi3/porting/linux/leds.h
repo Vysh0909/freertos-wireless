c79a742f32046e956e39242e693ec7e93370e921
Vyshnavi <menta-sai.vyshnavi@capgemini.com>
2025-11-13 14:49:11 +0530

    porting: add stub for struct led_trigger
    Added leds.h stub with minimal struct and no-op helpers to fix incomplete
    type errors for tx_led, rx_led, assoc_led, and radio_led in ieee80211_i.h.

--- FreeRTOS-raspi3/porting/linux/leds.h
+++ FreeRTOS-raspi3/porting/linux/leds.h
@@ -1,4 +1,21 @@
 #ifndef __LINUX_LEDS_H_
 #define __LINUX_LEDS_H_
 
+struct led_trigger {
+    const char *name;
+};
+
+/* Stub macros and functions (no-ops) */
+static inline int led_trigger_register(struct led_trigger *t)
+{
+    return 0;
+}
+
+static inline void led_trigger_unregister(struct led_trigger *t)
+{
+}
+
+static inline void led_trigger_event(struct led_trigger *t, int event)
+{
+}
 #endif /* __LINUX_LEDS_H_ */
